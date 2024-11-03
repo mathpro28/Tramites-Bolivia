cefe87861787309d92a039707294903883e8c91d
Mateo Mercado <matemerc@gmail.com>
2024-11-03 13:23:38 -0400

    Change the title and description.

--- Webpage/frontend/src/Components/ChatHeader.js
+++ Webpage/frontend/src/Components/ChatHeader.js
@@ -3,7 +3,7 @@ import './ChatHeader.css';
 
 const ChatHeader = () => (
   <div className="chat-header">
-    <h1>TramiBot AI Bolivia</h1>
+    <h1>TramiBot AI Bolivia NMP</h1>
     <p>Hacer <strong>tramites</strong> en Bolivia no tiene que ser tan <strong>feo</strong></p>
   </div>
 );
